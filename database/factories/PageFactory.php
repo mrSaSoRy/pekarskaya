f43f03e705a7a1b065e46c4dc7bea524a70d5968
Your Name <you@example.com>
2020-05-07 03:21:56 +1000

    Добавил сидр но он не работает из за проблем с categories_id

--- database/factories/PageFactory.php
+++ database/factories/PageFactory.php
@@ -6,12 +6,14 @@ use App\Models\Page;
 use Faker\Generator as Faker;
 
 $factory->define(Page::class, function (Faker $faker) {
-
+    $title = rtrim($faker->realText($faker->numberBetween(15,45)),".");
     return [
-        'categories_id' => $faker->word,
-        'title' => $faker->word,
-        'small_text' => $faker->word,
-        'text' => $faker->text,
+        'title' => $title,
+        'small_text' => $faker->text(rand(150, 300)),
+        'text' => $faker->text(rand(300, 500)),
+        'img' => $faker->imageUrl(750, 375),
+        'img_small' => $faker->imageUrl(80, 80),
+        'slug' => Str::slug($title),
         'created_at' => $faker->date('Y-m-d H:i:s'),
         'updated_at' => $faker->date('Y-m-d H:i:s'),
         'deleted_at' => $faker->date('Y-m-d H:i:s')
